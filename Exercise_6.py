c97c8cd18de70f7a34810ae1b69b614db0ecf21c
Ph6bia <56878935+Ph6bia@users.noreply.github.com>
2021-11-24 18:47:06 -0500

    Update Exercise_6.py

--- Exercise_6.py
+++ Exercise_6.py
@@ -1,5 +1,7 @@
-#Ask the user for a string and print out whether this string is a palindrome or not.
-#(A palindrome is a string that reads the same forwards and backwards.)
+'''
+Ask the user for a string and print out whether this string is a palindrome or not.
+(A palindrome is a string that reads the same forwards and backwards.)
+'''
 
 def is_palindrome(word):
     if word == word[::-1]:
