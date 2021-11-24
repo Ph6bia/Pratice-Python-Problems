e32393fb8f321a7db82325c00b9effae426a365d
Ph6bia <56878935+Ph6bia@users.noreply.github.com>
2021-11-24 18:48:26 -0500

    Update Exercise_5.py

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,3 +1,19 @@
+'''
+Take two lists, say for example these two:
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only 
+the elements that are common between the lists (without duplicates). 
+Make sure your program works on two lists of different sizes.
+
+Extras:
+    1)Randomly generate two lists to test this
+    2)Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+'''
+
+
+
+
 def find_commons(list_a, list_b):
     a = list_a
     b = list_b
@@ -6,4 +22,4 @@ def find_commons(list_a, list_b):
     list_of_commons = list(commons) #takes the list_of_commons and turns it into a list
     print(list_of_commons)
 
-find_commons( )
+find_commons( , )
