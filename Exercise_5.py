d51005114dd011cabad5df7a7d0a14ecfe4b1004
Ph6bia <56878935+Ph6bia@users.noreply.github.com>
2021-11-24 18:28:33 -0500

    Update Exercise_5.py

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,6 +1,9 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-a_set = set(a) #sets list a as variable1
-commons = a_set.intersection(b) #finds the commons between list a and list b
-list_of_commons = list(commons) #takes the list_of_commons and
-print(list_of_commons)
+def find_commons(list_a, list_b):
+    a = list_a
+    b = list_b
+    a_set = set(a) #sets list a as variable1
+    commons = a_set.intersection(b) #finds the commons between list a and list b
+    list_of_commons = list(commons) #takes the list_of_commons and turns it into a list
+    print(list_of_commons)
+
+find_commons( )
